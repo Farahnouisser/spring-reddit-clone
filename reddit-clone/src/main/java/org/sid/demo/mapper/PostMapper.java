0794230bb13fba5cd3fe304bf2a6a987e8e7b30f
farahnouisser <nouisserfarah@gmail.com>
2020-09-13 19:52:00 +0100

    API post

---
+++ reddit-clone/src/main/java/org/sid/demo/mapper/PostMapper.java
@@ -0,0 +1,33 @@
+package org.sid.demo.mapper;
+
+
+
+import java.util.Optional;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.sid.demo.dto.PostRequest;
+import org.sid.demo.dto.PostResponse;
+import org.sid.demo.model.Post;
+import org.sid.demo.model.Subreddit;
+import org.sid.demo.model.User;
+import org.sid.demo.service.AuthService;
+import org.springframework.beans.factory.annotation.Autowired;
+
+
+
+@Mapper(componentModel = "spring")
+public interface PostMapper {
+@Mapping(target = "createdDate", expression = "java(java.time.Instant.now())")
+@Mapping(target = "subreddit", source = "subreddit")
+@Mapping(target = "user", source = "user")
+@Mapping(target = "description", source = "postRequest.description")
+Post map(PostRequest postRequest, Subreddit subreddit, User user);
+@Mapping(target = "id", source = "postId")
+@Mapping(target = "postName", source = "postName")
+@Mapping(target = "description", source = "description")
+@Mapping(target = "url", source = "url")
+@Mapping(target = "subredditName", source = "subreddit.name")
+@Mapping(target = "userName", source = "user.username")
+PostResponse mapToDto(Post post);
+}
