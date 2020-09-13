32de006681f499e12960a30b829d0e77e4cb656f
farahnouisser <nouisserfarah@gmail.com>
2020-09-13 22:53:20 +0100

    Documment Rest Api using swagger and springfox

--- reddit-clone/src/main/java/org/sid/demo/RedditCloneApplication.java
+++ reddit-clone/src/main/java/org/sid/demo/RedditCloneApplication.java
@@ -1,14 +1,18 @@
 package org.sid.demo;
 
+import org.sid.demo.config.SwaggerConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.context.annotation.Import;
 import org.springframework.scheduling.annotation.EnableAsync;
+
+
 
 
 
 @SpringBootApplication
 @EnableAsync
+@Import(SwaggerConfiguration.class)
 
 public class RedditCloneApplication {
 
