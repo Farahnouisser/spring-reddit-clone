6de8d64130755aaea8db67102e10e74f7be1a821
farahnouisser <nouisserfarah@gmail.com>
2020-09-13 22:25:09 +0100

    vote api

---
+++ reddit-clone/src/main/java/org/sid/demo/service/VoteService.java
@@ -0,0 +1,57 @@
+package org.sid.demo.service;
+
+
+
+import java.util.Optional;
+
+import org.sid.demo.dto.VoteDto;
+import org.sid.demo.exceptions.PostNotFoundException;
+import org.sid.demo.exceptions.SpringRedditException;
+import org.sid.demo.model.Post;
+import org.sid.demo.model.Vote;
+import org.sid.demo.repository.PostRepository;
+import org.sid.demo.repository.VoteRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import static org.sid.demo.model.VoteType.UPVOTE;
+
+
+import lombok.AllArgsConstructor;
+
+@Service
+@AllArgsConstructor
+public class VoteService {
+
+    private final VoteRepository voteRepository;
+    private final PostRepository postRepository;
+    private final AuthService authService;
+
+    @Transactional
+    public void vote(VoteDto voteDto) {
+        Post post = postRepository.findById(voteDto.getPostId())
+                .orElseThrow(() 
+                		-> new PostNotFoundException("Post Not Found with ID - " + voteDto.getPostId()));
+        Optional<Vote> voteByPostAndUser = voteRepository.findTopByPostAndUserOrderByVoteIdDesc(post, authService.getCurrentUser());
+        if (voteByPostAndUser.isPresent() &&
+                voteByPostAndUser.get().getVoteType()
+                        .equals(voteDto.getVoteType())) {
+            throw new SpringRedditException("You have already "
+                    + voteDto.getVoteType() + "'d for this post");
+        }
+        if (UPVOTE.equals(voteDto.getVoteType())) {
+            post.setVoteCount(post.getVoteCount() + 1);
+        } else {
+            post.setVoteCount(post.getVoteCount() - 1);
+        }
+        voteRepository.save(mapToVote(voteDto, post));
+        postRepository.save(post);
+    }
+
+    private Vote mapToVote(VoteDto voteDto, Post post) {
+        return Vote.builder()
+                .voteType(voteDto.getVoteType())
+                .post(post)
+                .user(authService.getCurrentUser())
+                .build();
+    }
+}
